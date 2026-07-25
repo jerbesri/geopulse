759d986febcef6ef5d22ff379897b9f44607468d
Joseph Mackle <jos15070@esri.com>
2026-07-25 12:21:09 -0700

    starter widget

---
+++ client/your-extensions/widgets/run-suggestions/src/runtime/widget.tsx
@@ -0,0 +1,11 @@
+import { React, type AllWidgetProps } from "jimu-core";
+
+const Widget = (props: AllWidgetProps<any>) => {
+  return (
+    <div className="widget-starter jimu-widget">
+      This is your starter widget!
+    </div>
+  );
+};
+
+export default Widget;
