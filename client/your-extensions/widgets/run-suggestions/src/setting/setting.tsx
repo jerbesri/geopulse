7c719cb7fb012c0598098f12a65b678ff877c457
Jonathan Erb <145492178+jonathan-erb@users.noreply.github.com>
2026-07-26 00:37:27 -0700

    Revert "update window forecast"

--- client/your-extensions/widgets/run-suggestions/src/setting/setting.tsx
+++ client/your-extensions/widgets/run-suggestions/src/setting/setting.tsx
@@ -123,7 +123,6 @@ const Setting = (props: AllWidgetSettingProps<IMConfig>) => {
             <p className="mb-1">Forecast Window</p>
             <TextInput
               value={props.config?.forecastWindow ?? "next_24_hours"}
-              placeholder="next_hour, next_24_hours, or 2026-07-27T08:30:00.000Z"
               onChange={(event) => {
                 onConfigChange("forecastWindow", event.target.value);
               }}
